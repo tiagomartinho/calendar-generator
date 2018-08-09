88865808f1b4f084152fe88186faf9ccc429481f
Tiago Martinho <t.martinho@icloud.com>
2018-08-09 15:59:23 +0200

    Add Alarm at event time, extract parameters

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const { writeFileSync } = require('fs')
-var fs = require('fs');
-const ics = require('ics')
+const { writeFileSync } = require("fs");
+var fs = require("fs");
+const ics = require("ics");
+
+let startDate = [2018, 8, 9, 15, 59];
+let duration = { hours: 1, minutes: 30 };
+let title = "Bolder Boulder";
+let description = "Annual 10-kilometer run in Boulder, Colorado";
 
 const event = {
-  start: [2018, 5, 30, 6, 30],
-  duration: { hours: 6, minutes: 30 },
-  title: 'Bolder Boulder',
-  description: 'Annual 10-kilometer run in Boulder, Colorado',
-}
+  start: startDate,
+  duration: duration,
+  title: title,
+  description: description,
+  alarms: [
+    {
+      action: "display",
+      trigger: startDate
+    }
+  ]
+};
 
 ics.createEvent(event, (error, value) => {
   if (error) {
-    console.log(error)
+    console.log(error);
   }
-  fs.writeFileSync(`${__dirname}/event.ics`, value)
-})
+  fs.writeFileSync(`${__dirname}/event.ics`, value);
+});
